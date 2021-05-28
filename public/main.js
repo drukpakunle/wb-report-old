2b5871d9bc16903f8a4298e548e5e17e5a749f29
drukpakunle <gd13r6unz@relay.firefox.com>
2021-05-28 20:53:57 +0300

    [DK][flow]  changed the compiled report under autodeploy pages job;

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.getJSON('./static/groups.json', function (groupsJson) {
+    $.getJSON('./public/static/groups.json', function (groupsJson) {
         var stringified = JSON.stringify(groupsJson);
         var groups = JSON.parse(stringified);
         var select = $('<select class="form-control form-control-sm"></select>').attr("id", "groupSelect").attr("name", "groups");
@@ -10,7 +10,7 @@ $(document).ready(function () {
         $("#groupSelector").html(select);
     });
 
-    $.getJSON('./static/suites.json', function (suitesJson) {
+    $.getJSON('./public/static/suites.json', function (suitesJson) {
         var stringified = JSON.stringify(suitesJson);
         var suites = JSON.parse(stringified);
         var select = $('<select class="form-control form-control-sm"></select>').attr("id", "suiteSelect").attr("name", "suites");
@@ -20,7 +20,7 @@ $(document).ready(function () {
         $("#suiteSelector").html(select);
     });
 
-    $.getJSON('./static/branches.json', function (branchesJson) {
+    $.getJSON('./public//static/branches.json', function (branchesJson) {
         var stringified = JSON.stringify(branchesJson);
         var branches = JSON.parse(stringified);
         var select = $('<select class="form-control form-control-sm"></select>').attr("id", "branchSelect").attr("name", "branches");
@@ -69,7 +69,7 @@ $(function () {
 
         $.ajax({
             type: 'POST',
-            url: 'https://git.wildberries.ru/api/v4/projects/4682/ref/' + branch + '/trigger/pipeline?token=ee60cf17eb292db1de947c2aabf613&' + runParams,
+            url: 'https://' + branch + '/trigger/pipeline?token=' + runParams,
             processData: false,
             contentType: false,
             dataType: 'json',
@@ -94,14 +94,15 @@ function showPipelineErrorMessage(responseText) {
 
 function showPipelineSuccessMessage(pipelineUrl) {
     var pipelineLink = pipelineUrl;
-    var alertMessage = '<span>\
-                            Тесты запущены. Результат появится на это странице после завершения пайплайна:\
-                        </span>\
-                        <p>\
-                            <a href="' + pipelineLink + '" target="_blank">' + pipelineLink + '\
-                                <img class="icon-mini" src="static/icons/external-link-alt-solid.svg" />\
-                            </a>\
-                        </p>';
+    var alertMessage = '<span>' +
+                            'Тесты запущены. Результат появится на это странице после завершения пайплайна:' +
+                        '</span>' +
+                        '<p>' +
+                            '<a href="' + pipelineLink + '" target="_blank">' + pipelineLink +
+                              '<img class="icon-mini" src="/static/icons/external-link-alt-solid.svg" />' +
+                            '</a>'+
+                        '</p>';
+                        
     $("#alertMessage").html(alertMessage);
     $(".alert").attr('style', 'display: block;');
 };
@@ -110,51 +111,51 @@ $(document).ready(function () {
     let listDiv = document.getElementById('list');
     let table = listDiv.parentNode;
 
-    $.getJSON('history.json', function (historyJson) {
+    $.getJSON('./public/history.json', function (historyJson) {
         var stringified = JSON.stringify(historyJson);
         var pipelines = JSON.parse(stringified);
         console.log(pipelines);
 
         pipelines.sort((a, b) => b.pipelineId - a.pipelineId);
+
         pipelines.map(function (pipeline) {
             var runGroups = pipeline.runGroups == "" ? 'All' : pipeline.runGroups;
-            var reportUrl = pipeline.locale + '/' + pipeline.pipelineId;
+            var reportUrl = 'public/' + pipeline.locale + '/' + pipeline.pipelineId;
 
-            var pipelineLink = '<a href="https://git.wildberries.ru/mobile/site-tests/-/pipelines/' +
-                pipeline.pipelineId + '" target="_blank">' + pipeline.pipelineId +
-                '</a>';
+            var pipelineLink = '<a href="https://' +
+                    pipeline.pipelineId + '" target="_blank">' + pipeline.pipelineId + '</a>';
 
             var upstreamPipelineLink = pipeline.upstreamPipelineId == "null" ?
                 'manual' :
-                '<a href="https://git.wildberries.ru/mobile/site/-/pipelines/' +
-                pipeline.upstreamPipelineId + '" target="_blank">' + pipeline.upstreamPipelineId +
+                '<a href="https://' +
+                    pipeline.upstreamPipelineId + '" target="_blank">' + pipeline.upstreamPipelineId +
                 '</a>';
 
             var environmentUrl = '<a href="' + pipeline.envUrl + '" target="_blank">' + pipeline.envUrl + '</a>';
 
-            table.innerHTML += '<tr> \
-                    <td class="locale">\
-                        <img class="flagIcon" src="./static/flags/' + pipeline.locale + '.svg" title="' + pipeline.locale.toUpperCase() + '">\
-                    </td>\
-                    <td>' + pipeline.appVersion + '</td>\
-                    <td>' + pipeline.dateTime + '</td>\
-                    <td>\
-                        <a href="' + reportUrl + '" target="_blank">\
-                            <img class="icon-mini" src="./static/icons/external-link-alt-solid.svg" />\
-                        </a>\
-                    </td>\
-                    <td>' + pipelineLink + '</td>\
-                    <td>' + runGroups + '</td>\
-                    <td>' + environmentUrl + '</td>\
-                    <td>' + pipeline.envOverrideHost + '</td>\
-                    <td>' + pipeline.device + '</td>\
-                    <td>' + upstreamPipelineLink + '</td>\
-                    <td>\
-                        <a href="' + reportUrl + '/params.json" target="_blank">\
-                            <img class="icon-mini" src="./static/icons/external-link-alt-solid.svg" />\
-                        </a>\
-                    </td>\
-                </tr>';
+            table.innerHTML += '<tr>' +
+                    '<td class="locale">' +
+                        '<img class="flagIcon" src="./public/static/flags/' + pipeline.locale + '.svg" title="' + pipeline.locale.toUpperCase() + '">' +
+                    '</td>' +
+                    '<td>' + pipeline.appVersion + '</td>' +
+                    '<td>' + pipeline.dateTime + '</td>' +
+                    '<td>' +
+                        '<a href="' + reportUrl + '" target="_blank">' +
+                            '<img class="icon-mini" src="./public/static/icons/external-link-alt-solid.svg" />' +
+                        '</a>' +
+                    '</td>' +
+                    '<td>' + pipelineLink + '</td>' +
+                    '<td>' + runGroups + '</td>' +
+                    '<td>' + environmentUrl + '</td>' +
+                    '<td>' + pipeline.envOverrideHost + '</td>' +
+                    '<td>' + pipeline.device + '</td>' +
+                    '<td>' + upstreamPipelineLink + '</td>' +
+                    '<td>' +
+                        '<a href="' + reportUrl + '/params.json" target="_blank">' +
+                            '<img class="icon-mini" src="./public/static/icons/external-link-alt-solid.svg" />' +
+                        '</a>' +
+                    '</td>' +
+                '</tr>';
         })
     });
 });
